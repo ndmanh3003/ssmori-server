46135ee725cd110bd36da76df5b76dacf8931ee4
Duc Manh Nguyen <ndmanh3003@gmail.com>
2024-12-23 16:15:40 +0700

    feat(format): eslint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,87 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports', 'import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended'
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'import/no-unresolved': 'off',
+    curly: ['error', 'all'],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    quotes: ['error', 'single'],
+    'arrow-parens': ['error', 'always'],
+    'computed-property-spacing': ['error', 'never'],
+    'brace-style': 'error',
+    'no-irregular-whitespace': 'error',
+    indent: ['error', 2],
+    'comma-dangle': ['error', 'never'],
+    semi: ['error', 'never'],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_.*?$'
+      }
+    ],
+    'unused-imports/no-unused-imports': 'warn',
+    'object-curly-spacing': ['error', 'always'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'type',
+          'builtin',
+          'object',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index'
+        ],
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'external',
+            position: 'after'
+          }
+        ],
+        'newlines-between': 'always'
+      }
+    ],
+    'no-warning-comments': 'error',
+    'padding-line-between-statements': [
+      'warn',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return'
+      },
+      {
+        blankLine: 'always',
+        prev: ['const', 'let', 'var'],
+        next: '*'
+      },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var']
+      }
+    ]
+  }
+}
